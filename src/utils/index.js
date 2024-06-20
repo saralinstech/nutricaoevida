0deace82bd4101f667e8ccaa21f70f66dec6cff9
Sara Lins <saralins19@gmail.com>
2024-06-20 20:48:53 -0300

    ajustando ordem de imagens

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,3 +21,27 @@ export const healthyReasons = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
 ];
+
+export const testimonials = [
+  {
+    image: "/ProjectImages/TestimonialsSection/testimonial-img-2.png",
+    name: "Paulo Gorge",
+    years: "3 anos",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+  },
+  {
+    image: "/ProjectImages/TestimonialsSection/testimonial-img-1.png",
+    name: "Mariana Bittencourt",
+    years: "1 ano",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+  },
+  {
+    image: "/ProjectImages/TestimonialsSection/testimonial-img-3.png",
+    name: "Claudia Maria",
+    years: "5 anos",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+  },
+];
